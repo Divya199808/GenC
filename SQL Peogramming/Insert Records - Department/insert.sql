c102e4606518c9935e2db15be18d34aaa8e1acf6
Divya199808 <81949146+Divya199808@users.noreply.github.com>
2021-05-14 12:22:16 +0530

    Create insert.sql

---
+++ SQL Peogramming/Insert Records - Department/insert.sql
@@ -0,0 +1,3 @@
+insert into department(department_id,department_name,department_block_number) values (1,"CSE",3);
+insert into department(department_id,department_name,department_block_number) values (2,"IT",3);
+insert into department(department_id,department_name,department_block_number) values (3,"SE",3);
